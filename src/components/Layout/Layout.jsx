8a2b80dad0cdf8c8bb8c5c73e7bd759e3e156b62
Sldvlld <sladvlad@gmail.com>
2023-06-01 16:34:36 +0300

    change pages

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
-import AppBar from './AppBar.jsx';
+import AppBar from '../AppBar/AppBar.jsx';
 export default function Layout() {
   return (
     <>
