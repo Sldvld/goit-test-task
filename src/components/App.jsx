ef0ed33b84fe36f0b5d0cac443eabc9a3dd04614
Sldvlld <sladvlad@gmail.com>
2023-06-05 14:07:08 +0300

    add toast

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,23 @@
-import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
 import Layout from './Layout/Layout';
+import { lazy } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Home = lazy(() => import('../Pages/Home/Home'));
 const Tweets = lazy(() => import('../Pages/Tweets/Tweets'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/tweets" redirectTo="/tweets" element={<Tweets />} />
-      </Route>
-      <Route path="*" element={<Navigate to="/" replace />}></Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/tweets" redirectTo="/tweets" element={<Tweets />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />}></Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 };
